0f0db9a7fc8bd8c4806bd262e3473c9239dbfb55
Alex <fuckyobitch@Alexs-MacBook-Pro-2.local>
2018-10-19 16:37:31 -0600

    added animations to splash screen

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const mongodb = process.env.MONGODB_URI || 'localhost';
 mongoose.connect("mongodb://sudoUser:sudoUs3r@ds163402.mlab.com:63402/hs-leaderboard");
 
 mongoose.connection.on("connected", () => {
-    console.log("Mongoose is connected")
+    console.log("connected to dataBASS")
 });
 
 mongoose.connection.on("error", (err) => {
