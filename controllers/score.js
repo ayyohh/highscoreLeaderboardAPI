9606a1f0a93e2bc59f861f6a23a4b6a23b54fbb3
Alex <fuckyobitch@Alexs-MacBook-Pro-2.local>
2019-01-11 17:23:31 -0500

    fully functioning leaderboard

--- controllers/score.js
+++ controllers/score.js
@@ -16,15 +16,39 @@ router.get('/game', (req, res) => {
 
 
 
-//Index route
+//============= leaderboard calls =====================================
 router.get("/api/leaderboard", (req, res) => {
   Score.find({}, (err, foundScores) => {
     if(err){
       console.log('error in find');
       console.log(err);
     } else {
+      let sortedArr = []
+
+      for (let i = 0; i < foundScores.length; i++) {
+        sortedArr[i] = foundScores[i]
+
+      }
+      var flag = true;
+      let temp;
+      while(flag) {
+        flag = false;
+        for (let i = 0; i < sortedArr.length - 1; i++) {
+          if(sortedArr[i].score > sortedArr[i + 1].score) {
+            temp = sortedArr[i];
+            sortedArr[i] = sortedArr[i + 1];
+            sortedArr[i + 1] = temp;
+            flag = true;
+          }
+        }
+      }
+
+      console.log(sortedArr, 'this is sortedArr');
+
+
       res.render('../views/EscapeFromViews/leaderboard.ejs', {
-        scores: foundScores,
+
+        scores: sortedArr,
       });
     }
   });
@@ -45,6 +69,7 @@ router.post('/api/leaderboard', (req, res) => {
 
 
 //==================== api call for me ========================
+
 // Index Route
 router.get("/api/v1/leaderboard", async (req, res, next) => {
     try {
@@ -59,38 +84,6 @@ router.get("/api/v1/leaderboard", async (req, res, next) => {
     }
 });
 
-
-//
-// // New Route
-// router.post("/api/leaderboard", async (req, res) => {
-//     console.log("This is req.session in the post route of scoreController:", req.session);
-//     try {
-//         console.log("This is req.body in scoreController:", req.body);
-//         const createdScore = await Score.create(req.body);
-//         res.json({
-//             status: 200,
-//             data: createdScore
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         res.send(err);
-//     }
-// });
-//
-//
-// // Show Route
-// router.get("/:id", async (req, res, next) => {
-//     try {
-//         const foundScore = await Score.findById(req.params.id);
-//         res.json({
-//             status: 200,
-//             data: foundScore
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         res.send(err);
-//     }
-// });
 
 
 module.exports = router;
